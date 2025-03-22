c699b44e4b367f3d02ab3cae8d23865650c44bcd
coderconnoisseur <142653086+coderconnoisseur@users.noreply.github.com>
2025-03-23 01:36:18 +0530

    Email feature turned asynchronous

--- Projectdir/email.py
+++ Projectdir/email.py
@@ -2,19 +2,8 @@ from flask_mail import Message
 from flask import render_template
 from Projectdir import app
 from Projectdir import mail
+from threading import Thread
 
-def send_email(subject,sender,recipients,text_body,html_body):
-    msg=Message(subject=subject,sender=sender,recipients=recipients)
-    msg.body=text_body
-    msg.html=html_body
-    mail.send(msg)
-    # Debugging: Print email content before sending
-    print("\n--- Debugging Email Content ---")
-    print(f"Subject: {msg.subject}")
-    print(f"From: {msg.sender}")
-    print(f"To: {msg.recipients}")
-    print(f"Body:\n{msg.body}")
-    print("\n--- End of Email ---\n")
 
 def send_password_reset_email(user):
     token=user.get_reset_password_token()
@@ -23,3 +12,15 @@ def send_password_reset_email(user):
             recipients=[user.email],
             text_body=render_template('email/reset_password.txt',user=user,token=token),
             html_body=render_template('email/reset_password.html',user=user,token=token))
+
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject,sender,recipients,text_body,html_body):
+    msg=Message(subject=subject,sender=sender,recipients=recipients)
+    msg.body=text_body
+    msg.html=html_body
+    Thread(target=send_async_email,args=(app,msg)).start()
+
+
