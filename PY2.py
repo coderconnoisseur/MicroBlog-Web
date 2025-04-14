ccb186858e9a540c5a93e076411757a49b4b3420
coderconnoisseur <142653086+coderconnoisseur@users.noreply.github.com>
2025-04-14 12:08:35 +0530

    deployment reqs

--- PY2.py
+++ PY2.py
@@ -1,5 +1,9 @@
-from Projectdir import app,db
+from Projectdir import app, db
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 from Projectdir.models import User,Post
 @app.shell_context_processor
 def make_shell_context():
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PY2:app
